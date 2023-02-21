2f46c90598342b7279282886e32fab7feaa4389e
huynamboz <teahuynam2003@gmail.com>
2023-02-22 02:10:01 +0700

    login & register

---
+++ components/layout/header/header.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="header-container">
+        <div class="site-logo">
+            <img src="~/assets/img/logo-colored.png" alt="" class="logo-site">
+        </div>
+        <search />
+        <div class="list-action">
+            <div class="profile action-item">
+                <img src="~/assets/img/profile.png" alt="" class="icon-action">
+                <p>Cá nhân</p>
+            </div>
+            <div class="cart action-item">
+                <img src="~/assets/img/cart.png" alt="" class="icon-action">
+                <p>Giỏ hàng</p>
+                </div>
+                <div class="mess action-item">
+                    <img src="~/assets/img/message.png" alt="" class="icon-action">
+                    <p>Tin nhắn</p>
+                </div>
+        </div>
+    </div>
+</template>
+<script>
+import search from '~/components/layout/header/search.vue'
+export default {
+    name: 'headerTab',
+    components: {
+        search
+    }
+}
+</script>
+<style lang="scss" scoped>
+.logo-site {
+    height: 50px;
+
+}
+
+.header-container {
+    position: fixed;
+    width: 100%;
+    height: 70px;
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 50px;
+}
+.list-action{
+    display: flex;
+    gap: 20px;
+}
+.action-item{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    align-items: center;
+    font-size: 15px;
+    &:hover{
+        cursor: pointer;
+    }
+}
+.icon-action{
+    width: 25px;
+    height: 25px;
+}
+</style>
---
+++ components/layout/header/search.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="search-container">
+        <input type="text" placeholder="search" class="search-input">
+        <button class="btn-search">Search</button>
+    </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+button{
+    background-color: #0D6EFD;
+    color: white;
+    border: none;
+    margin: 0;
+    padding: 10px 20px;
+    border-radius: 3px;
+}
+input{
+    margin: 0;
+    all : unset;
+}
+.search-input{
+    border: none;
+    padding: 0px 20px;
+    border-radius: 6px;
+    width: 100%;
+}
+.search-container{
+    display: flex;
+    border: 2px solid #0D6EFD;
+    border-radius: 6px;
+    width: 500px;
+    height: 35px;
+}
+</style>
