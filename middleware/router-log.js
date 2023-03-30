4612152fc0245d6ba7bb3bec2572de83347027eb
huynamboz <teahuynam2003@gmail.com>
2023-03-30 11:53:24 +0700

    ok2

--- middleware/router-log.js
+++ middleware/router-log.js
@@ -1,7 +1,4 @@
 export default function (context){
     console.log("[LOG]: Middleware router-log.js is running")
-	if (!context.$cookies.get("auth.user")?.value){
-		console.log("[LOG]: User is logged in")
-		context.$auth.setUser(JSON.parse(context.$cookies.get('auth.user')));
-	}
+	context.$auth.setUser(JSON.parse(localStorage.getItem('auth.user')))
 }
---
+++ public/_nuxt/3b43e12.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{382:function(n,e,d){"use strict";d.r(e);var l=d(22),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
