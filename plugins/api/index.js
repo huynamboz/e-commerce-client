88de29630532e4fc7bc711d495895ebde3da37f3
huynamboz <teahuynam2003@gmail.com>
2023-05-29 10:48:56 +0700

    chore: refactor axios

--- plugins/api/index.js
+++ plugins/api/index.js
@@ -1,11 +1,34 @@
 import Product from "./products";
 import Auth from "./auth";
-import apiToken from './apiHaveToken';
 import User from "./users";
+import axios from 'axios'
 export default (context, inject, store) => {
+	// init axios instance
+	const instance = axios.create({
+		baseURL: process.env.BASE_URL_API,
+		headers: {
+			common: {
+				'Content-Type': 'application/json',
+			},
+		},
+	});
+
+	// set token for axios instance
+	instance.interceptors.request.use(
+		config => {
+			const token = localStorage.getItem('auth._token.local');
+			if (token) {
+				config.headers['Authorization'] = token;
+			}
+				return config;
+			},
+		error => {
+				return Promise.reject(error);
+			}
+		);
 	inject("api", {
-		products: Product(apiToken),
-		auth: Auth(apiToken),
-		users: User(apiToken),
+		products: Product(instance),
+		auth: Auth(instance),
+		users: User(instance),
 	});
 };
